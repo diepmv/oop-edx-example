d41c2745b681ebbc28a4e099230dbddedb2f8c3a
Mai Diep <maidiep.ptit@gmail.com>
2018-02-13 15:48:59 +0700

    add mit professor

--- mitperson.py
+++ mitperson.py
@@ -40,3 +40,16 @@ class TransferStudent(Student):
 
 def isStudent(obj):
   return isinstance(obj, Student)
+
+
+class Professor(MITPerson):
+  def __init__(self, name, department):
+    MITPerson.__init__(self, name):
+    self.department = department
+
+  def speak(self, utterance):
+    new = 'In course ' + self.department + ' we say '
+    return MITPerson.speak(self, new + utterance)
+
+  def lecture(self, topic):
+    return self.speak("It is obbious that " + topic)
